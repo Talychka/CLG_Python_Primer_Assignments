23e2c8329f22f8796be482013c28291ba394f5a2
Talya Mathews <talya.mathews@dpc.vic.gov.au>
2024-04-28 03:48:31 +1000

    final homework lesson 10

--- python_lesson_10/bing_password_app.py
+++ python_lesson_10/bing_password_app.py
@@ -2,25 +2,33 @@ import tkinter as tk
 class Root(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.label_1 = tk.Label(self, text='Enter a password (your favourite language): ',
+        
+        self.label_1 = tk.Label(self, text='Enter a password (your favourite coding language): ',
             fg='#ddf3f5', bg='#e36387')
         self.label_1.pack()
+        
         self.entry_0=tk.Entry(self, show='*', bg='#ddf3f5')
         self.entry_0.pack()
-        self_label_2 = tk.Label(self, fg='#a6dcef', bg='#e36387')
+        
+        self.label_2=tk.Label(self, fg='#a6dcef', bg='#e36387')
         self.label_2.pack()
+        
         self.btn = tk.Button(self, text = 'submit', 
             command=self.submit)
         self.btn.pack()
+
         self.correct_pass = 'python'
+    
     def submit(self):
-        self,pswd = self.entry_0.get()
+        self.pswd = self.entry_0.get()
         if self.pswd == self.correct_pass:
-            self.label_2['text'] = 'Congratulations!'
+            self.label_2['text'] = 'Great answer!'
         else:
-            self.label_2['text'] = 'Try again!'
+            self.label_2['text'] = 'Disappointing answer, try again!'
         
 if __name__ == '__main__':
     root = Root()
+
     root['bg'] = '#e36387'
+    
     root.mainloop()
